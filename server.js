4263b326e47253e36f660197f62c2e451c660dcc
Brandon Sievert (slick) <brandonwsievert@gmail.com>
2022-03-29 14:39:06 -0400

    Removed functions and declared summoner

--- server.js
+++ server.js
@@ -14,37 +14,19 @@ app.get("/", (req, res) => {
   res.send("Welcome message successful");
 });
 
-// functions
-async function getAccountCredentials() {
-    
-    await axios.get(accountURL).then((response) => {
-        const puuid = response.data.puuid
-        return puuid;
-    })
-}
-
-async function getAccountMatchList() {
-    
-    await axios.get(matchListURL).then((response) => {
-        const accountMatchList = response.data
-        return accountMatchList
-    })
-}
 
 // client sends summoner name -> server hits riot endpoints -> backend sends data to client
 app.post("/summoner/riot/compiled-data/", async (req, res) => {
   try {
+    const summoner = req.body.summoner;
     const accountURL = `${routeNA1}lol/summoner/v4/summoners/by-name/${summoner}${devKey}`;
     axios.get(accountURL).then((response) => {
-        const matchListURL = `${routeNA1}lol/match/v5/matches/by-puuid/${response.data.puuid}/ids${devKey}`;
-        if (response)
-            axios.get(matchListURL).then((response) => {
-                res.status(200).send(response.data)
-            })
-    })
-
-
-
+      const matchListURL = `${routeNA1}lol/match/v5/matches/by-puuid/${response.data.puuid}/ids${devKey}`;
+      if (response)
+        axios.get(matchListURL).then((response) => {
+          res.status(200).send(response.data);
+        });
+    });
   } catch (error) {
     res.status(500).send({
       message: error.message,
