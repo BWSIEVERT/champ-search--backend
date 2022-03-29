c9fe327eaadfa5d51489af1d2bf60c2796df5972
Brandon Sievert (slick) <brandonwsievert@gmail.com>
2022-03-29 14:07:42 -0400

    Changed response handling for match endpoint

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const {
-    devKey,
-    routeNA1
-} = require('./environmentConfig');
-const { default: axios } = require('axios');
+const express = require("express");
+const cors = require("cors");
+const { devKey, routeNA1 } = require("./environmentConfig");
+const { default: axios } = require("axios");
 const app = express();
 const PORT = 9000;
 
@@ -12,38 +9,33 @@ const PORT = 9000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Welcome message successful')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome message successful");
+});
 
 // client sends summoner name -> server hits riot endpoints -> backend sends data to client
-app.post('/summoner/riot/compiled-data/', async (req, res) => {
-    try {
-        const summoner = req.body.summoner
-        const accountURL = `${routeNA1}lol/summoner/v4/summoners/by-name/${summoner}${devKey}`
-        
-        await axios.get(accountURL)
-            .then((response) => {
-                const summonerCredentials = response.data
-                const puuid = summonerCredentials.puuid
-                const matchListURL = `${routeNA1}lol/match/v5/matches/by-puuid/${puuid}/ids${devKey}`
-                await axios.get(matchListURL)
-                        .then((response) => {
-                            res.status(200).send(response.data)
-                        })
-            })
+app.post("/summoner/riot/compiled-data/", async (req, res) => {
+  try {
+    const summoner = req.body.summoner;
+    const accountURL = `${routeNA1}lol/summoner/v4/summoners/by-name/${summoner}${devKey}`;
 
-        // await axios.get()
+    await axios.get(accountURL).then((response) => {
+      const puuid = response.data.puuid;
+      const matchListURL = `${routeNA1}lol/match/v5/matches/by-puuid/${puuid}/ids${devKey}`;
+      return axios.get(matchListURL).then((response) => {
+          res.status(200).send(response.data)
+      })
+    });
 
-    } catch (error) {
-        res.status(500).send({
-            message: error.message
-        })
-    }
-})
-
+    // await axios.get()
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
 
 //
 app.listen(process.env.PORT || 9000, () => {
-    console.log(`*** Server is running! ***`)
-})
+  console.log(`*** Server is running! ***`);
+});
