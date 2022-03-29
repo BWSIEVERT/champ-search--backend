3af3b6f43682b19b986876d26eed7fbb89e5c6b4
Brandon Sievert (slick) <brandonwsievert@gmail.com>
2022-03-29 16:01:04 -0400

    restructure of code to handle single requests

--- server.js
+++ server.js
@@ -14,23 +14,38 @@ app.get("/", (req, res) => {
   res.send("Welcome message successful");
 });
 
-
-// client sends summoner name -> server hits riot endpoints -> backend sends data to client
-app.post("/summoner/riot/compiled-data/", async (req, res) => {
+// client sends summoner name -> server hits riot endpoint -> server sends puuid to client
+app.post("/summoner/riot/", async (req, res) => {
   try {
     const summoner = req.body.summoner;
     const accountURL = `${routeNA1}lol/summoner/v4/summoners/by-name/${summoner}${devKey}`;
-    const accountResponse = await axios.get(accountURL)
-    const matchListURL = `${routeNA1}lol/match/v5/matches/by-puuid/${accountResponse.data.puuid}/ids${devKey}`;
-    await axios.get(matchListURL).then((response) => {
-        res.status.send(response.data)
-    })
+    await axios.get(accountURL).then((response) => {
+      res.status(200).send(response.data.puuid);
+    });
   } catch (error) {
-    res.status(500).send({
-      message: error.message,
+    res.status(404).send({
+      errorMessage: error.message,
+      errorDescription: `A summoner with the name of ${req.body.summoner} could not be found.`
     });
   }
 });
+
+// client sends puuid -> server hits riot endpoint -> server sends match list to client
+app.post("/summoner/matches/riot/", async (req, res) => {
+    try{
+        const puuid = req.body.puuid
+        const matchListURL = `${routeNA1}lol/match/v5/matches/by-puuid/${accountResponse.data.puuid}/ids${devKey}`;
+        await axios.get(matchListURL).then((response) => {
+            res.status(200).send(response.data)
+        })
+    } catch(error) {
+        res.status(404).send({
+            errorMessage: error.message,
+            errorDescription: `A match list with the puuid of ${req.body.puuid} could not be found.`
+        })
+    }
+    
+})
 
 //
 app.listen(process.env.PORT || 9000, () => {
